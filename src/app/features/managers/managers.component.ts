ea393650fa21e69c65b23553f7217c68bdf7f027
Rafael <rtoresani@gmail.com>
2025-05-05 19:45:39 -0300

    add: modal on create manager

--- src/app/features/managers/managers.component.ts
+++ src/app/features/managers/managers.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ListManagersComponent } from "./components/list-managers/list-managers.component";
 import { CreateManagerComponent } from "./components/create-manager/create-manager.component";
+import { ModalService } from '@app/shared/services/modal.service';
+import { CommonModule } from '@angular/common';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroMagnifyingGlass, heroPlusCircle } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-managers',
-  imports: [ListManagersComponent, CreateManagerComponent],
+  imports: [ListManagersComponent, CommonModule, NgIcon],
+    providers: [provideIcons({ heroPlusCircle, heroMagnifyingGlass })],
   templateUrl: './managers.component.html',
   styleUrl: './managers.component.scss'
 })
 export class ManagersComponent {
+  _modalService = inject(ModalService)
 
+  createManager() {
+    this._modalService.open(CreateManagerComponent);
+  }
 }
